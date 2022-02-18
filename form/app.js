f8f488cba1e396a91f631b7a6e8516c1ba5043eb
firmsclan <firas.khaldi1@gmail.com>
2022-02-18 09:21:10 -0800

    used app newLI when press enter

--- form/app.js
+++ form/app.js
@@ -1,5 +1,10 @@
 const form = document.querySelector("#shelterForm");
+const input = document.querySelector("#catName")
+const list = document.querySelector("#cats")
 form.addEventListener('submit', function(e){
-    e.preventDefault();
-    console.log("submited")
+ e.preventDefault();
+ const catName = input.value;
+ const newLI = document.createElement("LI")
+ newLI.innerText = catName;
+ list.append(newLI)
 })
