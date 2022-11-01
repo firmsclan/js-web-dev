e187d4a0c06bc337c05dfd58a7a178c0b12823ef
firmslan <firas.khaldi1@gmail.com>
2022-11-01 14:17:19 -0400

    inital check in

--- callbacks/app.js
+++ callbacks/app.js
@@ -1,44 +1,5 @@
-// setTimeout(()=>{
-//   document.body.style.backgroundColor = 'red'
-//   setTimeout(()=>{
-//     document.body.style.backgroundColor = 'orange'
-//     setTimeout(()=>{
-//         document.body.style.backgroundColor = 'orange'
-//       },1000)
-//   },1000)
-// },1000)
+const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
-//  const delayedColorChange = (newColor, delay) => {
-//     setTimeout(()=>{
-//     document.body.style.backgroundColor = newColor
-//      })
-//  }
-
-// delayedColorChange('olive',3000)
-// delayedColorChange('teal', 6000)
-
-// delayedColorChange('red',1000,() => {
-//     delayedColorChange('blue',1000,() => {
-//         delayedColorChange('green',1000,() => {
-//             delayedColorChange('orange',1000,() => {
-//                 delayedColorChange('brown',1000,() => {
-    
-//                 })
-//             })
-//         })
-//     })
-// })
-
-
-fakeRequestPromise('yelp.com/api/coffe/page1').then(() =>{
-    console.log("it worded!! (page 1")
-    console.log(data)
-    return fakeRequestPromise('yelp.com/api/coffe/page2')
+numbers.forEach(function(el){
+    console.log(el)
 })
-.then(()=>{
-    console.log("it worded!! (page 3")
-    return fakeRequestPromise('Oh,no a requesst failed')
-}
-)
-
-const login = aysnc (username, password)
